6c929976d6c44d02df796de0834b7da829f10f61
Lin shizai <linshizai.me@gmail.com>
2018-11-22 18:14:55 +0800

    Add example for HDWallet

---
+++ Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Linsz on 2018/11/22.
+//  Copyright © 2018 ONEROOT PROJECT. All rights reserved.
+//
+
+import UIKit
+import HDWallet
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
